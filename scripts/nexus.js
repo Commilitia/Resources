53dec60c0fa4246455b81974e0ae22dd9d4e8b17
Cheng Jingxuan <hbxycjx@outlook.com>
2024-05-09 19:51:11 +0800

    Initial Release

---
+++ scripts/nexus.js
@@ -0,0 +1,154 @@
+const nexusStyleExcludedCode=[
+    'code','pre','samp','pre *','kbd','tt',
+    'datalist','progress','meter','var'
+].join(', ');
+
+function nexusGetShadowColor(node){
+    const rgbToHsl=color=>{
+        const [r,g,b]=color.map(c=>c/255);
+        const max=Math.max(r,g,b), min=Math.min(r,g,b);
+        let h,s,l=(max+min)/2;
+
+        if(max===min)h=s=0;
+        else{
+            const d=max-min;
+            s=l>0.5?d/(2-max-min):d/(max+min);
+            switch(max){
+                case r:h=(g-b)/d+(g<b?6:0);break;
+                case g:h=(b-r)/d+2;break;
+                case b:h=(r-g)/d+4;break;
+            }
+            h/=6;
+        }
+
+        return [h,s,l];
+    }
+
+    const hslToRgb=color=>{
+        const [h,s,l]=color;
+        let r,g,b;
+
+        if(s===0)r=g=b=l;
+        else{
+            const hueToRgb=(p,q,t)=>{
+                if(t<0)t+=1;
+                if(t>1)t-=1;
+                if(t<1/6)return p+(q-p)*6*t;
+                if(t<1/2)return q;
+                if(t<2/3)return p+(q-p)*(2/3-t)*6;
+                return p;
+            }
+
+            const q=l<0.5?l*(1+s):l+s-l*s;
+            const p=2*l-q;
+            r=hueToRgb(p,q,h+1/3);
+            g=hueToRgb(p,q,h);
+            b=hueToRgb(p,q,h-1/3);
+        }
+
+        return [r,g,b].map(c=>Math.round(c*255));
+    }
+
+    const rgbToHex=color=>`#${color.map(c=>c.toString(16).padStart(2,'0')).join('')}`;
+
+    const parseColor=colorStr=>{
+        let match;
+        if(colorStr.startsWith('rgba')){
+            match=colorStr.match(/rgba\((\d+),\s*(\d+),\s*(\d+),\s*(\d+\.?\d*)\)/);
+            return [parseInt(match[1]),parseInt(match[2]),parseInt(match[3]),parseFloat(match[4])*255];
+        }else if(colorStr.startsWith('rgb')){
+            match=colorStr.match(/rgb\((\d+),\s*(\d+),\s*(\d+)\)/);
+            return [parseInt(match[1]),parseInt(match[2]),parseInt(match[3]),255];
+        }else if(colorStr.startsWith('#')){
+            switch(colorStr.length){
+                case 9:return [
+                    parseInt(colorStr.slice(1,3),16),
+                    parseInt(colorStr.slice(3,5),16),
+                    parseInt(colorStr.slice(5,7),16),
+                    parseInt(colorStr.slice(7,9),16)
+                ];
+                case 7:return [
+                    parseInt(colorStr.slice(1,3),16),
+                    parseInt(colorStr.slice(3,5),16),
+                    parseInt(colorStr.slice(5,7),16),
+                    255
+                ];
+                case 5:return [
+                    parseInt(colorStr[1]+colorStr[1],16),
+                    parseInt(colorStr[2]+colorStr[2],16),
+                    parseInt(colorStr[3]+colorStr[3],16),
+                    parseInt(colorStr[4]+colorStr[4],16)
+                ];
+                case 4:return [
+                    parseInt(colorStr[1]+colorStr[1],16),
+                    parseInt(colorStr[2]+colorStr[2],16),
+                    parseInt(colorStr[3]+colorStr[3],16),
+                    255
+                ];
+            }
+        }
+        return [0,0,0,255];
+    }
+
+    const color=parseColor(getComputedStyle(node).color);
+
+    let [r,g,b,a]=color;
+
+    let {h,s,l}=rgbToHsl([r,g,b]);
+    l=0.486*l+0.514;
+    a=a*0.87;
+
+    [r,g,b]=hslToRgb([h,s,l]);
+    return rgbToHex([r,g,b])+Math.round(a).toString(16).padStart(2,'0');
+}
+
+function nexusApplyStyle(node){
+    if(node.matches(nexusStyleExcludedCode)){
+        node.style.fontFamily='MonoLisa Commilitia, CommilitiaFont UI SC';
+        node.style.webkitTextStroke='0.08px';
+        node.style.textShadow=`0.45px 0.45px 0 ${nexusGetShadowColor(node)}`;
+        return; 
+    }
+    node.style.fontFamily='CommilitiaFont UI SC';
+    node.style.webkitTextStroke='0.08px';
+    node.style.textShadow=`0.45px 0.45px 0 ${nexusGetShadowColor(node)}`;
+}
+
+function nexusRecursiveExecute(node,selector,func){
+    func(node);
+    node.querySelectorAll(selector).forEach(child=>func(child));
+}
+
+function nexusSelectInScope(node,selector){
+    return node.matches(selector)?node:node.querySelector(selector);
+}
+
+document.head.appendChild((()=>{
+    const link=document.createElement('link');
+    link.rel='stylesheet';
+    link.type='text/css';
+    link.href='https://cdn.jsdelivr.net/gh/Commilitia/Resources/styles/nexus.css'
+    return link;
+})());
+
+document.addEventListener('DOMContentLoaded',async()=>{
+    await Promise.resolve();
+    document.documentElement.style.setProperty('--entry-content-text-color','#000000');
+    document.querySelectorAll('*').forEach(node=>{
+        if(!(node instanceof HTMLElement))return;
+        nexusApplyStyle(node);
+    });
+    new MutationObserver(mutations=>{
+        mutations.forEach(mutation=>{
+            mutation.addedNodes.forEach(node=>{
+                if(!(node instanceof HTMLElement))return;
+                nexusRecursiveExecute(node,'*',nexusApplyStyle);
+                let nodeX;
+                nodeX=nexusSelectInScope(node,'.post-footer div p');
+                if(nodeX)nodeX.innerHTML=nodeX.innerHTML.replace(/Q\.E\.D\./g,'Quad erat demonstrandum.');
+                nodeX=nexusSelectInScope(node,'.post-license');
+                if(nodeX)nodeX.parentElement.removeChild(nodeX);
+            });
+        });
+    }).observe(document.body,{attributes:false,childList:true,subtree:true});
+});
